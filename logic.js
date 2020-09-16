40aafb4071fc90f761aca08045b1dbc411feb286
Chad Tarpey <chadtarpey@gmail.com>
2020-09-15 19:17:49 -0500

    got the two api calls working

--- logic.js
+++ logic.js
@@ -1,8 +1,26 @@
 const apiKey = "dccc626c7bbab4fb50200a2eecb2a4af"
 const token = "3649349035133511"
 
-//https://comicvine.gamespot.com/api/search/?api_key=3adfda20d076cfbd8b85d5400f732cac58810057&format=json&sort=name:asc&resources=issue&query=
-
+var granimInstance = new Granim({
+    element: '#canvas-image-blending',
+    direction: 'top-bottom',
+    isPausedWhenNotInView: true,
+    image : {
+        source: "../assets/venom.jpeg",
+        blendingMode: 'multiply'
+    },
+    states : {
+        "default-state": {
+            gradients: [
+                ['#29323c', '#485563'],
+                ['#FF6B6B', '#556270'],
+                ['#80d3fe', '#7ea0c4'],
+                ['#f0ab51', '#eceba3']
+            ],
+            transitionSpeed: 7000
+        }
+    }
+});
 function searchComics() {
    
         var queryURL = "https://gateway.marvel.com:443/v1/public/characters?apikey=dccc626c7bbab4fb50200a2eecb2a4af"
@@ -15,16 +33,21 @@ function searchComics() {
     })
 }
 
-// var superHeroURL = "https://superheroapi.com/api/" + token;
+function searchStats() {
 
-// $.ajax({
-//     url: superHeroURL,
-//     method: "GET"
-// }).then(function(resp) {
-// console.log(resp);
-// })
+var superHeroURL = "https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/3649349035133511/search/batman";
+
+$.ajax({
+    url: superHeroURL,
+    method: "GET"
+}).then(function(resp) {
+console.log(resp);
+})
+}
+
 $("#searchComic").on("click", searchComics);
-$("#comic-view").on("click", searchComics);
+$("#stats").on("click", searchStats);
 
-
-//https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/825632581144352/search/batman
+var html = document.querySelector('html');
+html.style.backgroundImage = "../assets/venom.jpeg";
+html.style.backgroundSize = 'cover';
